ecd689b4f2cb365f448b581b7f577892a3616cb0
Martin Pugh <pugh@s3kr.it>
2020-02-28 18:48:38 +0100

    actually error when we get errors back from matrix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,19 @@ structure_message() {
   else
     body=$(jq -Rs --arg body "$1" --arg formatted_body "$2" '{"msgtype": "m.text", $body, "format": "org.matrix.custom.html", $formatted_body}' < /dev/null)
   fi
-  echo $body
+  echo "$body"
 }
 
 # send_message $body (json formatted) $room_id $access_token
 send_message() {
-curl -XPOST -d "$1" "https://$INPUT_SERVER/_matrix/client/r0/rooms/$2/send/m.room.message?access_token=$3"
+  out="$(curl -XPOST -d "$1" "https://$INPUT_SERVER/_matrix/client/r0/rooms/$2/send/m.room.message?access_token=$3")"
+  if [ "$(echo "$out" | jq .errcode)" = "null" ]; then
+    echo "[+] Message sent!"
+  else
+    echo '[!] Something went wrong sending the message'
+    echo "$out" | jq '.error'
+    exit 1
+  fi
 }
 
-msg_body=$(cat <<EOF
-EOF
-)
-
-# Created formatted body for clients that support it (???)
-formatted_msg_body=$(cat <<EOF
-EOF
-)
-
-send_message "$(structure_message "$INPUT_MESSAGE" "$INPUT_FORMATTED_MESSAGE")" $INPUT_ROOM_ID $INPUT_ACCESS_TOKEN
+send_message "$(structure_message "$INPUT_MESSAGE" "$INPUT_FORMATTED_MESSAGE")" "$INPUT_ROOM_ID" "$INPUT_ACCESS_TOKEN"
